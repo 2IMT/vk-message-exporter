d5e97bd004516f16ecef2229f12e1a774bfbb6ca
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-02 10:15:19 +0300

    Add stories table to schema

--- src/db/sql.h
+++ src/db/sql.h
@@ -148,6 +148,9 @@ CREATE TABLE IF NOT EXISTS audio_playlist_audios (
     audio_playlist_id INTEGER NOT NULL
 );
 
+CREATE INDEX IF NOT EXISTS idx_audio_playlist_audios_audio_id_audio_playlist_id
+    ON audio_playlist_audios(audio_id, audio_playlist_id);
+
 CREATE TABLE IF NOT EXISTS graffitis (
     id INTEGER NOT NULL PRIMARY KEY,
     owner_id INTEGER NOT NULL
@@ -161,8 +164,12 @@ CREATE TABLE IF NOT EXISTS money_requests (
     currency TEXT NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_audio_playlist_audios_audio_id_audio_playlist_id
-    ON audio_playlist_audios(audio_id, audio_playlist_id);
+CREATE TABLE IF NOT EXISTS stories (
+    id INTEGER NOT NULL PRIMARY KEY,
+    owner_id INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    expires_at INTEGER NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS message_attachments (
     message_from_id INTEGER NOT NULL,
@@ -359,6 +366,15 @@ SELECT EXISTS (SELECT * FROM money_requests WHERE id = ?1);
     static inline const std::string insert_money_request = R""""(
 INSERT INTO money_requests (id, from_id, to_id, amount, currency)
 VALUES (?1, ?2, ?3, ?4, ?5);
+)"""";
+
+    static inline const std::string exists_story = R""""(
+SELECT EXISTS (SELECT * FROM stories WHERE id = ?1);
+)"""";
+
+    static inline const std::string insert_story = R""""(
+INSERT INTO stories (id, owner_id, date, expires_at)
+VALUES (?1, ?2, ?3, ?4);
 )"""";
 
     static inline const std::string insert_message_attachment = R""""(
